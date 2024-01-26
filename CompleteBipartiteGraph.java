048459021b336293f0303b8bdcfd15159d6326eb
MeruzhanS <meruzhansargsyan48@gmail.com>
2024-01-25 16:11:27 -0800

    added comments

--- CompleteBipartiteGraph.java
+++ CompleteBipartiteGraph.java
@@ -1,3 +1,13 @@
+/** 
+* This graph extends the graph superclass to implement a unique type of classic
+* graph. This graph has to parts: A and B. All vertecies in A is connected to
+* all vertecies in B.
+*
+* HOWEVER: The key feature of this graph is that no two vertecies in A share an
+*          edge and no two vertecies in B share an edge.
+*
+* @author Meruzhan Sargsyan
+*/
 import java.util.ArrayList;
 
 class CompleteBipartiteGraph extends Graph {
@@ -9,6 +19,15 @@ class CompleteBipartiteGraph extends Graph {
     private final int STARTING_ROW = 50;
     private final int ROW_SPACING = 50;
 
+    /** 
+     * Constructor that creates an instance of superclass Graph and initializes
+     * field variables. 
+     *
+     * Calls generateVertecies and generateEdges and displayGraph
+     *
+     * @param Asize int size of part A (left column)
+     * @param Bsize int size of part B (right column)
+     */
     BipartiteGraph(int Asize, int Bsize) {
         super();
         this.Asize = Asize;
@@ -18,6 +37,12 @@ class CompleteBipartiteGraph extends Graph {
         displayGraph(new Display(this), this);
     }
 
+    /** 
+     * Generates vertecies into two separate columns.
+     * Part A is on the left column, Part B is on the right column.
+     *
+     * @param numVertecies int irrelevent in this method
+     */
     @Override
     void generateVertecies(int numVertecies) {
         int currentRow = STARTING_ROW;
@@ -33,6 +58,9 @@ class CompleteBipartiteGraph extends Graph {
 
     } 
 
+    /** 
+    * Connects all vertecies in Part A to all vertecies in Part B
+    */
     @Override 
     void generateEdges() {
         for(int i = 0; i < Asize; i++) {
