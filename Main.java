823efd2f6ca3b8bd75ad262cdb85a24d400b729d
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-06-18 10:43:22 -0700

    UI stuff finished

--- Main.java
+++ Main.java
@@ -4,42 +4,7 @@ import java.awt.event.*;
 
 class Main {
     public static void main(String args[]) {
-        // "java Main -c (complete) (n)"
-        // "java Main -c (complete) -b (bipartate) (A,B)"
         initWindow();
-        /*if(args.length == 0) {
-            System.out.println("Improper usage type -h for help");
-            return;
-        }
-        if(args[0].equals("-c")) {
-            if(args[1].equals("-b") ) {
-                if(args.length != 3) {
-                    System.out.println("Improper usage type -h for help");
-                    return;
-                }
-                try {
-                    int A = Integer.parseInt(args[2].substring(0, 1));
-                    int B = Integer.parseInt(args[2].substring(2, 3));
-                    new CompleteBipartiteGraph(A,B);
-                } catch(Exception ex) {
-                    System.out.println("Input error");
-                }
-                
-            } else {
-                try {
-                    int n = Integer.parseInt(args[1]);
-                     new CompleteGraph(n);
-                } catch(Exception ex) {
-                    System.out.println("Please enter a number");
-                    return;
-                }
-            }
-        } else if(args[0].equals("-h")) {
-            System.out.println("Usage:");
-            System.out.println("Complete graph of n: '-c {number}");
-            System.out.println("Complete Bipartite Graph of (A,B):" + 
-                "'-c -b {number1},{number2}");
-        }*/
     }
 
     public static void initWindow() {
@@ -72,17 +37,23 @@ class Main {
     }
 
     public static void parseInput(String inputType, String inputNum) {
-        switch(inputType) {
-            case "complete":
-                new CompleteGraph(Integer.parseInt(inputNum));
+
+        try {
+            switch(inputType) {
+                case "complete":
+                    new CompleteGraph(Integer.parseInt(inputNum));
+                    break;
+                case "complete bipartite":
+                    String g1 = inputNum.substring(0, inputNum.indexOf(","));
+                    String g2 = inputNum.substring(inputNum.indexOf(",") + 1, inputNum.length());
+                    new CompleteBipartiteGraph(Integer.parseInt(g1), Integer.parseInt(g2));
+                
                 break;
-            case "complete bipartite":
-                try {
-                    new CompleteBipartiteGraph(Integer.parseInt(inputNum), Integer.parseInt(inputNum));
-                } catch(Exception ex) {
-                    System.out.println("Error");
-                }
-                break;
+            }
+        } catch(Exception ex) {
+            String errorMessage = "Usage: \n" + "Complete Graph: Enter a number of vertecies only\n" +
+                "Complete Bipartite: Enter two numbers A and B in the format: A,B"; 
+            JOptionPane.showMessageDialog(null, errorMessage);
         }
     }
 }
